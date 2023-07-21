d53a01e312e1387b3e5cb30ab6925ad36733abba
Bor333 <mugen999999999@gmail.com>
2023-07-21 18:20:20 +0700

    GET

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+var_dump($_GET);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<form method="GET">
+    <input type="text" name="login">
+    <input type="password" name="pass">
+    <input type="submit" value="отправить">
+</form>
+</body>
+</html>
